358176eadee0f49314ca5bcb325e6cb4d6477932
erdem ay <eirdemmoon@gmail.com>
2022-10-28 16:15:53 +0300

    add

--- script.js
+++ script.js
@@ -5,12 +5,15 @@ let lName = document.querySelector(".lname");
 let age = document.querySelector(".age");
 let emailInp = document.querySelector(".email");
 let addBtn = document.querySelector(".button");
+let updateBtn = document.querySelector(".buttonUpdate");
 let tab = document.querySelector(".tab");
+let elm="";
+let allTodos = [];
 
 async function getFetch() {
   const response = await fetch("http://localhost:3000/posts");
   let res = await response.json();
-  console.log(res);
+  allTodos = res;
   showPost(res);
 }
 
@@ -32,7 +35,6 @@ async function postFetch() {
   });
   let res = await response.json();
   showPost(res);
-  console.log(data, res);
 }
 
 addBtn.addEventListener("click", (event) => {
@@ -55,11 +57,62 @@ const showPost = (posts) => {
       "</td>" +
       "<td>" +
       element.email +
-      "<td>"+
-      "<button class='editBtn'>"+'Edit'+"</button>" + 
-      "<button class='delBtn'>"+'Delete'+"</button>" ;
+      "<td>" +
+      "<button class='editBtn' onclick='editButton(" +
+      element.id +
+      ");'>" +
+      "Edit" +
+      "</button>" +
+      "<button class='delBtn' onclick='delButton(" +
+      element.id +
+      ");'>" +
+      "Delete" +
+      "</button>";
     ekleme += "</tr>";
     tab.innerHTML += ekleme;
   });
+
   console.log(posts);
+};
+
+async function delButton(id) {
+  const response = await fetch(`http://localhost:3000/posts/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+  });
+  let res = await response.json();
+}
+
+async function editButton(id) {
+  elm = allTodos.find((element) => element.id == id);
+  console.log(elm);
+  fName.value = elm.firstname;
+  lName.value = elm.lastname;
+  age.value = elm.age;
+  emailInp.value = elm.email;
+  addBtn.style.display = "none";
+  updateBtn.style.display = "block";
+}
+
+updateBtn.onclick = async (event) => {
+  event.preventDefault();
+  let data = {
+    firstname: fName.value,
+    lastname: lName.value,
+    age: age.value,
+    email: emailInp.value,
+  };
+  console.log(data);
+  const response = await fetch(`http://localhost:3000/posts/${elm.id}`, {
+    method: "PUT",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+  });
+  console.log(response);
+  addBtn.style.display = "block";
+  updateBtn.style.display = "none";
 };
